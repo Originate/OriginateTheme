cef9aa3f6f51c73d0eb462556b9eca226c84eb6a
dannychhay <danny.chhay@originate.com>
2017-02-13 17:19:33 -0800

    Add test for resolving ref, clean up

--- OriginateTheme/Scripts/ot_reference_resolver_test.py
+++ OriginateTheme/Scripts/ot_reference_resolver_test.py
@@ -17,8 +17,7 @@ pp = pprint.PrettyPrinter(indent=4)
 
 class ResolveComponentReferencesTestCase(unittest.TestCase):
     def runTest(self):
-        data = json.loads("""
-        {
+        data = json.loads("""{
           "colors": {
             "primary": "FFFF00"
           },
@@ -34,11 +33,33 @@ class ResolveComponentReferencesTestCase(unittest.TestCase):
               "tabItemFont": "$fonts.default"
             }
           }
-        }
-        """
-        )
+        }""")
         expectedData = copy.deepcopy(data)
         expectedData["components"]["tabBar"]["backgroundColor"] = data["colors"]["primary"]
         expectedData["components"]["tabBar"]["tabItemFont"] = data["fonts"]["default"]
         resolveComponentReferencesInData(data)
         self.assertEqual(data, expectedData)
+
+class ResolveReferenceTestCase(unittest.TestCase):
+    def runTest(self):
+        data = json.loads("""{
+          "colors": {
+            "primary": "FFFF00"
+          },
+          "fonts": {
+            "default" : {
+                "name" : "HelveticaNeue",
+                "size" : "14.0"
+            }
+          },
+          "components": {
+            "tabBar": {
+              "backgroundColor": "$colors.primary",
+              "tabItemFont": "$fonts.default"
+            }
+          }
+        }""")
+        aRef = data["components"]["tabBar"]["backgroundColor"]
+        resolvedRefData = resolveRef(aRef, data)
+        expectedData = data["colors"]["primary"]
+        self.assertEqual(resolvedRefData, expectedData)
