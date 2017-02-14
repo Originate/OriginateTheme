f4f843d2b3ccf13c5fe9061706c707befb0f31f3
dannychhay <danny.chhay@originate.com>
2017-02-14 11:36:52 -0800

    Make file output to a given directory, clean up

--- OriginateTheme/Scripts/ot_reference_resolver.py
+++ OriginateTheme/Scripts/ot_reference_resolver.py
@@ -1,47 +1,58 @@
+"""
+    ot_reference_resolver.py
+    Script which resolves references in OriginateTheme class files in a JSON file.
+
+    Arguments
+    ----------
+        -i: String
+            The input JSON file e.g. `Theme.json`.
+        -o: String
+            The directory where the files should be created in.
+"""
+
 import json
 import os
 import re
 import string
 import sys
+from ot_generator import parseArguments
 
-def main(argv):
+def resolveJSONFile(inputFile, outputDirectory):
     """
-        Method responsible for the program execution.
+        Method for resolving references in an OriginateTheme JSON file.
 
         Parameters
         ----------
-        argv: Array
-            Array containing all programs' arguments
+        inputFile: String
+            Path to the .json file containing the theme definitions.
+        outputDirectory: String
+            Path to the directory where the resolved file should be generated.
     """
-
-    # Extract the inputFile and outputDirectory.
-    (inputFile) = parseArguments(argv)
-
-    # Open the .JSON file and parse the fonts, colors and components.
     with open(inputFile, 'r') as file:
         try:
             data = json.load(file, 'utf-8')
             resolveComponentReferencesInData(data)
-            inputFile.write(data)
-            inputFile.close()
-
+            filePath = outputDirectory + inputFile
+            if os.path.isfile(filePath):
+                os.chmod(filePath, 0755)
+            with open(filePath, 'wb') as outputFile:
+                outputFile.write(json.dumps(data, indent=2))
         except ValueError:
             print 'Decoding JSON input file failed!'
             sys.exit()
 
-
 def resolveRef(aRef, data):
-    # """
-    #     Method for resolving specific referenced values in components against a source object.
-    #
-    #     Parameters
-    #     ----------
-    #     data: Object
-    #         The source object that defines the components, colors, fonts, etc.
-    #     aRef: String
-    #         The value of a component that references other properties such as colors and fonts
-    #         Must follow format: "$property.specificPropertyName".  Ex. "$colors.primary"
-    # """
+    """
+        Method for resolving specific referenced values in components against a source object.
+
+        Parameters
+        ----------
+        data: Object
+            The source object that defines the components, colors, fonts, etc.
+        aRef: String
+            The value of a component that references other properties such as colors and fonts
+            Must follow format: "$property.specificPropertyName".  Ex. "$colors.primary"
+    """
     m = re.search('\$(\w+).(\w+)', aRef)
     try:
         (parentKey, key) = (m.group(1), m.group(2))
@@ -54,17 +65,33 @@ def resolveRef(aRef, data):
         sys.exit()
 
 def resolveComponentReferencesInData(data):
-    # """
-    #     Method for resolving referenced values in components from a JSON object.
-    #
-    #     Parameters
-    #     ----------
-    #     data: Object
-    #         Object containing color, font, and component defintions. Each component definition
-    #         can use referenced values from the property definitions.  Ex: a color value of $colors.primary
-    # """
+    """
+        Method for resolving referenced values in components from a JSON object.
+
+        Parameters
+        ----------
+        data: Object
+            Object containing color, font, and component defintions. Each component definition
+            can use referenced values from the property definitions.  Ex: a color value of $colors.primary
+    """
     components = data['components']
     resolvedComponents = {}
     for componentName, componentProperties in components.iteritems():
         for key, aRef in componentProperties.iteritems():
             data["components"][componentName][key] = resolveRef(aRef, data)
+
+def main(argv):
+    """
+        Method responsible for the program execution.
+
+        Parameters
+        ----------
+        argv: Array
+            Array containing all programs' arguments
+    """
+
+    (inputFile, outputFile) = parseArguments(argv)
+    resolveJSONFile(inputFile, outputFile)
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
--- OriginateTheme/Scripts/ot_reference_resolver_test.py
+++ OriginateTheme/Scripts/ot_reference_resolver_test.py
@@ -13,27 +13,27 @@ import pprint
 
 from ot_reference_resolver import *
 
-pp = pprint.PrettyPrinter(indent=4)
+testJSONString = """{
+  "colors": {
+    "primary": "FFFF00"
+  },
+  "fonts": {
+    "default" : {
+        "name" : "HelveticaNeue",
+        "size" : "14.0"
+    }
+  },
+  "components": {
+    "tabBar": {
+      "backgroundColor": "$colors.primary",
+      "tabItemFont": "$fonts.default"
+    }
+  }
+}"""
 
 class ResolveComponentReferencesTestCase(unittest.TestCase):
     def runTest(self):
-        data = json.loads("""{
-          "colors": {
-            "primary": "FFFF00"
-          },
-          "fonts": {
-            "default" : {
-                "name" : "HelveticaNeue",
-                "size" : "14.0"
-            }
-          },
-          "components": {
-            "tabBar": {
-              "backgroundColor": "$colors.primary",
-              "tabItemFont": "$fonts.default"
-            }
-          }
-        }""")
+        data = json.loads(testJSONString)
         expectedData = copy.deepcopy(data)
         expectedData["components"]["tabBar"]["backgroundColor"] = data["colors"]["primary"]
         expectedData["components"]["tabBar"]["tabItemFont"] = data["fonts"]["default"]
@@ -42,23 +42,7 @@ class ResolveComponentReferencesTestCase(unittest.TestCase):
 
 class ResolveReferenceTestCase(unittest.TestCase):
     def runTest(self):
-        data = json.loads("""{
-          "colors": {
-            "primary": "FFFF00"
-          },
-          "fonts": {
-            "default" : {
-                "name" : "HelveticaNeue",
-                "size" : "14.0"
-            }
-          },
-          "components": {
-            "tabBar": {
-              "backgroundColor": "$colors.primary",
-              "tabItemFont": "$fonts.default"
-            }
-          }
-        }""")
+        data = json.loads(testJSONString)
         aRef = data["components"]["tabBar"]["backgroundColor"]
         resolvedRefData = resolveRef(aRef, data)
         expectedData = data["colors"]["primary"]
