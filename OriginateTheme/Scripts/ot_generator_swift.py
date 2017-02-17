9ecd662f732bbc20d56752ac5920b48dc6fc70ad
dannychhay <danny.chhay@originate.com>
2017-02-16 17:15:39 -0800

    Fix code generation, update tests

--- OriginateTheme/Scripts/ot_generator_swift.py
+++ OriginateTheme/Scripts/ot_generator_swift.py
@@ -1,13 +1,27 @@
 import string
+from ot_generator import upcaseFirstLetter
 
 structTemplate = """public struct $name {
 $body
 }"""
+
 varTemplate = """var $name: $type"""
+
+otLazyVarTemplate = """\
+lazy var $name: $type = {
+    return $type(dictionary: self.dictionary)
+}()"""
+
 computedStaticPropertyTemplate ="""public $var {
     return $comp
 }"""
+
+computedStaticPropertyTemplate ="""public $var {
+    return $comp
+}"""
+
 otComputationTemplate = """$convenience("$path", dictionary: $dict, fallback: $fb)"""
+
 fontTemplate = """UIFont(name: "$fontName", size: $fontSize)"""
 
 otThemeObjectInitTemplate = """public init(dictionary: ThemeDefinition = [:]) {
@@ -21,6 +35,10 @@ tabLength = 4
 def varWith(varName, varType):
     return string.Template(varTemplate).substitute({'name': varName, 'type': varType})
 
+def otLazyComponentVar(varName, varType = None):
+    theType = varType if varType else upcaseFirstLetter(varName)
+    return string.Template(otLazyVarTemplate).substitute({'name': varName, 'type': theType})
+
 def otComputationWith(convenienceConstructor, keyPath, dictionary, fallback):
     return string.Template(otComputationTemplate).substitute({'convenience': convenienceConstructor, 'path': keyPath, 'dict': dictionary, 'fb': fallback})
 
@@ -33,15 +51,46 @@ def structWithBody(structName, structBody):
 def indent(string, numSpaces):
     return  '\n'.join(["".rjust(numSpaces)+s for s in string.split('\n')])
 
+def uiColorWithColor(color):
+    return "UIColor.color(\"%s\")" % color.code
+
 def createColorProperty(color):
-    value = otComputationWith('UIColor.color', 'colors.'+color.key, 'dictionary', color.code)
+    value = otComputationWith('UIColor.color', 'colors.'+color.key, 'dictionary', uiColorWithColor(color))
     return computedPropertyWith(color.key, 'UIColor', value)
+
+def NSNumberWithBool(bool):
+    return "NSNumber(value: %s)" % str(bool.boolValue).lower()
+
+def cgPointValue(point):
+    return "NSValue(cgPoint: CGPoint(x: %s, y: %s))" % (point.x, point.y)
+
+def uiEdgeInsetsValueWithInsets(insets):
+    return "NSValue(uiEdgeInsets: UIEdgeInsets(top: %s, left: %s, bottom: %s, right: %s))" % (insets.top, insets.left, insets.bottom, insets.right)
+
+def cgRectValueWithRect(rect):
+    return "NSValue(cgRect: CGRect(x: %s, y: %s, width: %s, height: %s))" % (rect.x, rect.y, rect.width, rect.height)
+
+def createBoolProperty(bool):
+    value = otComputationWith('NSNumber.number', 'bool.'+bool.key, 'dictionary', NSNumberWithBool(bool))
+    return computedPropertyWith(bool.key, 'Bool', value+'.boolValue')
+
+def createPointProperty(point):
+    value = otComputationWith('NSValue.value', 'point.'+point.key, 'dictionary', cgPointValue(point))
+    return computedPropertyWith(point.key, 'CGPoint', value+'.cgPointValue')
+
+def createUIEdgeInsetProperty(insets):
+    value = otComputationWith('NSValue.value', 'insets.'+insets.key, 'dictionary', uiEdgeInsetsValueWithInsets(insets))
+    return computedPropertyWith(insets.key, 'UIEdgeInsets', value+'.uiEdgeInsetsValue')
+
+def createCGRectProperty(rect):
+    value = otComputationWith('NSValue.value', 'rects.'+rect.key, 'dictionary', cgRectValueWithRect(rect))
+    return computedPropertyWith(rect.key, 'CGRect', value+'.cgRectValue')
 
 def swiftFontInstanceWith(font):
     return string.Template(fontTemplate).substitute({'fontName': font.name, 'fontSize': font.size})
 
 def createFontProperty(font):
-    value = otComputationWith('UIFont.font', 'fonts.'+font.key, 'dictionary', swiftFontInstanceWith(font))
+    value = otComputationWith('UIFont.font', 'fonts.'+font.key, 'dictionary', swiftFontInstanceWith(font)+'!')
     return computedPropertyWith(font.key, 'UIFont', value)
 
 def structWithItems(structName, implementationItems):
@@ -60,3 +109,45 @@ def otThemObjectFactoryInit(type, dictionary):
 
 def setValueStatement(receiver, aProperty, value):
     return "%s.%s = %s" % (receiver, aProperty, value)
+
+otThemeObjectDictionaryVar = varWith('dictionary', 'ThemeDefinition')
+
+def createColorStruct(color):
+    implementationItems = [otThemeObjectDictionaryVar, createColorProperty(color), otThemeObjectInitWithItems()]
+    return structWithItems("Colors", implementationItems)
+
+def createFontStruct(font):
+    implementationItems = [otThemeObjectDictionaryVar, createFontProperty(font), otThemeObjectInitWithItems()]
+    return structWithItems("Fonts", implementationItems)
+
+def createOTStructWithImplemenationItems(structName, items):
+    actualItems = [otThemeObjectDictionaryVar]
+    actualItems.extend(items)
+    actualItems.append(otThemeObjectInitWithItems())
+    return structWithItems(structName, actualItems)
+
+def createComponentStruct(component):
+    # for every component, make a struct
+    structName = upcaseFirstLetter(component.key)
+    fonts = [createFontProperty(f) for f in component.fonts]
+    colors = [createColorProperty(c) for c in component.colors]
+    bools = [createBoolProperty(b) for b in component.bools]
+    points = [createPointProperty(p) for p in component.points]
+    insets = [createUIEdgeInsetProperty(i) for i in component.insets]
+    rects = [createCGRectProperty(r) for r in component.rects]
+
+    componentVars = []
+    componentVars.extend(fonts)
+    componentVars.extend(colors)
+    componentVars.extend(bools)
+    componentVars.extend(points)
+    componentVars.extend(insets)
+    componentVars.extend(rects)
+    return (createOTStructWithImplemenationItems(structName, componentVars))
+
+def createAggregatedComponents(name, components):
+    componentsInterface = [otLazyComponentVar(comp.key) for comp in components]
+    componentImplementations = [createComponentStruct(comp) for comp in components]
+    componentsStruct = [createOTStructWithImplemenationItems('Components', componentsInterface)]
+    componentsStruct.extend(componentImplementations)
+    return '\n\n'.join(componentsStruct)
--- OriginateTheme/Scripts/ot_generator_swift_test.py
+++ OriginateTheme/Scripts/ot_generator_swift_test.py
@@ -13,81 +13,62 @@ import json
 from ot_generator_swift import *
 from ot_generator import *
 
-navigationBarStruct = """public struct navigationBar {
-    public var backgroundColor: UIColor {
-        return UIColor.color("colors.red", dictionary: dictionary, fallback: UIColor.green)
-    }
-    public var font: UIFont {
-        return UIFont.font(keyPath: "fonts.text", dictionary: dictionary, fallback: UIFont.systemFont(ofSize: 16.0))
-    }
-}"""
-navigationBarBody = """    public var backgroundColor: UIColor {
-        return UIColor.color("colors.red", dictionary: dictionary, fallback: UIColor.green)
-    }
-    public var font: UIFont {
-        return UIFont.font(keyPath: "fonts.text", dictionary: dictionary, fallback: UIFont.systemFont(ofSize: 16.0))
-    }"""
-
 varName = "backgroundColor"
 varType = "UIColor"
-
-exColorStruct = """public struct Colors {
-    var dictionary: ThemeDefinition
-    \n\
-    public var red: UIColor {
-        return UIColor.color("colors.red", dictionary: dictionary, fallback: UIColor.green)
-    }
-    \n\
-    public init(dictionary: ThemeDefinition = [:]) {
-        self.dictionary = dictionary
-    }
-}"""
-
-exFontStruct = """public struct Fonts {
-    var dictionary: ThemeDefinition
-    \n\
-    public var text: UIFont {
-        return UIFont.font("fonts.text", dictionary: dictionary, fallback: UIFont(name: "HelveticaNeue", size: 15))
-    }
-    \n\
-    public init(dictionary: ThemeDefinition = [:]) {
-        self.dictionary = dictionary
-    }
-}"""
-
-exInitItem = """public init(dictionary: ThemeDefinition = [:]) {
-    self.dictionary = dictionary
-    self.navigationBar = NavigationBar(dictionary: dictionary)
-}"""
 
 exComponentsStruct = """public struct Components {
     var dictionary: ThemeDefinition
     \n\
-    var navigationBar: NavigationBar
+    lazy var navigationBar: NavigationBar = {
+        return NavigationBar(dictionary: self.dictionary)
+    }()
     \n\
     public init(dictionary: ThemeDefinition = [:]) {
         self.dictionary = dictionary
-        self.navigationBar = NavigationBar(dictionary: dictionary)
+    }
+}
+
+public struct NavigationBar {
+    var dictionary: ThemeDefinition
+    \n\
+    public var description: UIFont {
+        return UIFont.font("fonts.description", dictionary: dictionary, fallback: UIFont(name: "HelveticaNeue-Light", size: 12.0)!)
     }
     \n\
-    public struct NavigationBar {
-        var dictionary: ThemeDefinition
-        \n\
-        public var red: UIColor {
-            return UIColor.color("colors.red", dictionary: dictionary, fallback: UIColor.green)
-        }
-        \n\
-        public var text: UIFont {
-            return UIFont.font("fonts.text", dictionary: dictionary, fallback: UIFont(name: "HelveticaNeue", size: 15))
-        }
-        \n\
-        public init(dictionary: ThemeDefinition = [:]) {
-            self.dictionary = dictionary
-        }
+    public var text: UIFont {
+        return UIFont.font("fonts.text", dictionary: dictionary, fallback: UIFont(name: "HelveticaNeue-Light", size: 14.0)!)
+    }
+    \n\
+    public var background: UIColor {
+        return UIColor.color("colors.background", dictionary: dictionary, fallback: UIColor.color("84E0FAFF"))
+    }
+    \n\
+    public var tint: UIColor {
+        return UIColor.color("colors.tint", dictionary: dictionary, fallback: UIColor.color("000000FF"))
+    }
+    \n\
+    public var isTranslucent: Bool {
+        return NSNumber.number("bool.isTranslucent", dictionary: dictionary, fallback: NSNumber(value: true)).boolValue
+    }
+    \n\
+    public var iconOffset: CGPoint {
+        return NSValue.value("point.iconOffset", dictionary: dictionary, fallback: NSValue(cgPoint: CGPoint(x: 15, y: 20))).cgPointValue
+    }
+    \n\
+    public var textInsets: UIEdgeInsets {
+        return NSValue.value("insets.textInsets", dictionary: dictionary, fallback: NSValue(uiEdgeInsets: UIEdgeInsets(top: 15, left: 20, bottom: 10, right: 5))).uiEdgeInsetsValue
+    }
+    \n\
+    public var elementFrame: CGRect {
+        return NSValue.value("rects.elementFrame", dictionary: dictionary, fallback: NSValue(cgRect: CGRect(x: 15, y: 20, width: 10, height: 5))).cgRectValue
+    }
+    \n\
+    public init(dictionary: ThemeDefinition = [:]) {
+        self.dictionary = dictionary
     }
 }"""
 
-otThemeObjectDictionaryVar = varWith('dictionary', 'ThemeDefinition')
+
 
 def failureMessageWith(expected, actual):
     return '\nexpected: ' + expected + '!=\n' + 'actual: ' + actual
@@ -100,24 +81,27 @@ class VarTestCase(unittest.TestCase):
 
 class OriginateThemePropertyComputationTestCase(unittest.TestCase):
     def runTest(self):
-        expected = "UIColor.color(\"colors.red\", dictionary: dictionary, fallback: UIColor.green)"
-        data = otComputationWith("UIColor.color", "colors.red", "dictionary", "UIColor.green")
+        expected = "UIColor.color(\"colors.red\", dictionary: dictionary, fallback: UIColor.green!)"
+        data = otComputationWith("UIColor.color", "colors.red", "dictionary", "UIColor.green!")
         self.assertEqual(expected, data)
 
 class StaticPublicComputedPropertyTestCase(unittest.TestCase):
     def runTest(self):
         expected = """public var backgroundColor: UIColor {
-    return UIColor.color("colors.red", dictionary: dictionary, fallback: UIColor.green)
+    return UIColor.color("colors.red", dictionary: dictionary, fallback: UIColor.color("FF0000")!)
 }"""
-        computation  =  otComputationWith("UIColor.color", "colors.red", "dictionary", "UIColor.green")
+        computation  =  otComputationWith("UIColor.color", "colors.red", "dictionary", "UIColor.color(\"FF0000\")!")
         data = computedPropertyWith(varName, varType, computation)
         self.assertEqual(expected, data)
 
 class StructTestCase(unittest.TestCase):
     def runTest(self):
-        expected = navigationBarStruct
-        structName = "navigationBar"
-        body = navigationBarBody
+        expected = """\
+public struct NavigationBar {
+    body
+}"""
+        structName = "NavigationBar"
+        body = "    body"
         data = structWithBody(structName, body)
 
         self.assertEqual(expected, data, msg = failureMessageWith(expected, data))
@@ -125,15 +109,15 @@ class StructTestCase(unittest.TestCase):
 class ColorPropertyTestCase(unittest.TestCase):
     def runTest(self):
         expected = """public var primary: UIColor {
-    return UIColor.color("colors.primary", dictionary: dictionary, fallback: UIColor.green)
+    return UIColor.color("colors.primary", dictionary: dictionary, fallback: UIColor.color("FF0000"))
 }"""
-        data = createColorProperty(Color('primary', 'UIColor.green'))
+        data = createColorProperty(Color('primary', 'FF0000'))
         self.assertEqual(expected, data, msg = failureMessageWith(expected, data))
 
 class FontPropertyTestCase(unittest.TestCase):
     def runTest(self):
         expected = """public var text: UIFont {
-    return UIFont.font("fonts.text", dictionary: dictionary, fallback: UIFont(name: "HelveticaNeue", size: 15))
+    return UIFont.font("fonts.text", dictionary: dictionary, fallback: UIFont(name: "HelveticaNeue", size: 15)!)
 }"""
         data = createFontProperty(Font('text', 'HelveticaNeue', 15))
         self.assertEqual(expected, data, msg = failureMessageWith(expected, data))
@@ -149,21 +133,47 @@ class IndentTestCase(unittest.TestCase):
 
 class CreateColorsStructTestCase(unittest.TestCase):
     def runTest(self):
-        expected = exColorStruct
-        implementationItems = [otThemeObjectDictionaryVar, createColorProperty(Color('red', 'UIColor.green')), otThemeObjectInitWithItems()]
-        data = structWithItems("Colors", implementationItems)
+        expected = """\
+public struct Colors {
+    var dictionary: ThemeDefinition
+    \n\
+    public var red: UIColor {
+        return UIColor.color("colors.red", dictionary: dictionary, fallback: UIColor.color("FF0000"))
+    }
+    \n\
+    public init(dictionary: ThemeDefinition = [:]) {
+        self.dictionary = dictionary
+    }
+}"""
+        color = Color('red', 'FF0000')
+        data = createColorStruct(color)
         self.assertEqual(expected, data, msg=failureMessageWith(expected,data))
 
 class CreateFontsStructTestCase(unittest.TestCase):
     def runTest(self):
-        expected = exFontStruct
-        implementationItems = [otThemeObjectDictionaryVar, createFontProperty(Font('text', 'HelveticaNeue', 15)), otThemeObjectInitWithItems()]
-        data = structWithItems("Fonts", implementationItems)
+        expected = """\
+public struct Fonts {
+    var dictionary: ThemeDefinition
+    \n\
+    public var text: UIFont {
+        return UIFont.font("fonts.text", dictionary: dictionary, fallback: UIFont(name: "HelveticaNeue", size: 15)!)
+    }
+    \n\
+    public init(dictionary: ThemeDefinition = [:]) {
+        self.dictionary = dictionary
+    }
+}"""
+        font = Font('text', 'HelveticaNeue', 15)
+        data = createFontStruct(font)
         self.assertEqual(expected, data, msg=failureMessageWith(expected,data))
 
 class ThemeObjectInitExtraTestCase(unittest.TestCase):
     def runTest(self):
-        expected = exInitItem
+        expected = """\
+public init(dictionary: ThemeDefinition = [:]) {
+    self.dictionary = dictionary
+    self.navigationBar = NavigationBar(dictionary: dictionary)
+}"""
         items = [otThemeObjectStandardInitLine, "self.navigationBar = NavigationBar(dictionary: dictionary)"]
         data = otThemeObjectInitWithItems(items)
         self.assertEqual(expected, data, msg=failureMessageWith(expected,data))
@@ -171,16 +181,7 @@ class ThemeObjectInitExtraTestCase(unittest.TestCase):
 class CreateComponentsTestCase(unittest.TestCase):
     def runTest(self):
         expected = exComponentsStruct
-        navBarName = 'NavigationBar'
-        componentVars = [varWith('dictionary', 'ThemeDefinition'), varWith('navigationBar', navBarName)]
-        init = otThemeObjectInitWithItems([otThemeObjectStandardInitLine, "self.navigationBar = NavigationBar(dictionary: dictionary)"])
-
-        navigationBarImplementationItems = [otThemeObjectDictionaryVar, createColorProperty(Color('red', 'UIColor.green')), createFontProperty(Font('text', 'HelveticaNeue', 15)), otThemeObjectInitWithItems()]
-        navigationBarStruct = structWithItems(navBarName, navigationBarImplementationItems)
-
-        items = []
-        items.extend(componentVars)
-        items.append(init)
-        items.append(navigationBarStruct)
-        data = structWithItems('Components',items)
+        jsonData = json.loads('{ "navigationBar" : { "colors" : { "background" : "84E0FA", "tint" : "000000" }, "fonts" : { "text" : { "name" : "HelveticaNeue-Light", "size" : 14.0 }, "description" : { "name" : "HelveticaNeue-Light", "size" : 12.0 } }, "isTranslucent" : true, "iconOffset":  { "x": 15, "y": 20 }, "textInsets": { "top": 15, "left": 20, "bottom": 10, "right": 5 }, "elementFrame": { "x": 15, "y": 20, "width": 10, "height": 5 } } }')
+        components = parseComponents(jsonData)
+        data = createAggregatedComponents('Components',components)
         self.assertEqual(expected, data, msg=failureMessageWith(expected,data))
