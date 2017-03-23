639cb09869dfc924665ff642659390727a518f2c
Allen Wu <allen.wu@originate.com>
2017-03-23 15:27:47 -0700

    Fix font size lookup

--- OriginateTheme/Sources/Swift/Extensions/UIFont+KeyPath.swift
+++ OriginateTheme/Sources/Swift/Extensions/UIFont+KeyPath.swift
@@ -10,9 +10,9 @@ import UIKit
 
 extension UIFont {
     class func font(_ keyPath: String, dictionary: [String: Any], fallback: UIFont) -> UIFont {
-        let size = Float(dictionary[keyPath: KeyPath("\(keyPath).size")] as? String ?? "0.0") ?? Float(UIFont.systemFontSize)
+        let size = Float(dictionary[keyPath: KeyPath("\(keyPath).size")] as? String ?? "\(fallback.pointSize)") ?? Float(UIFont.systemFontSize)
         let name = dictionary[keyPath: KeyPath(keyPath)] as? String ?? fallback.fontName
-        
+
         return UIFont(name: name, size: CGFloat(size)) ?? fallback
     }
 }
